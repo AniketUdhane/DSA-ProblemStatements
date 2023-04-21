21ac891bc88d66b0e99f6055d03b0dbad3ed1e2e
AniketUdhane <96518558+AniketUdhane@users.noreply.github.com>
2023-04-21 12:30:52 +0530

    Create string_largestCommonPrefix.cpp

---
+++ DSA/C++/string_largestCommonPrefix.cpp
@@ -0,0 +1,46 @@
+/*
+
+14. Longest Common Prefix
+Easy
+13.5K
+3.9K
+Companies
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+
+*/
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size()==0){
+            return "";
+        }
+        int ans = INT_MAX;
+        for(int i=0;i<strs.size()-1;i++){
+            ans = min(ans, (int)min(strs[i].size(),strs[i+1].size()));
+            while(strs[i].substr(0,ans)!=strs[i+1].substr(0,ans)){
+                ans--;
+            }
+            if(ans == 0){
+                return "";
+            }
+        }
+            return strs[0].substr(0,ans);
+        
+    }
+};
+
